dceceacb4a54ee701a64eed70e2280645a235f5c
mariusz79 <mpawlowic@gmail.com>
2019-04-24 03:27:34 +0100

    connect to mongodb

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI1')  # 'MONGO_URI1' jest zapisane w enviroment variables, zeby nie bylo widac hasla
+
+
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks = mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
